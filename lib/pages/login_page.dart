06b73000c1c50ced66f1a95c0b82ac120782677d
Princekumar-Mehta <2001.mehta.2001@gmail.com>
2022-01-07 19:49:58 +0530

    day 6

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,14 @@
+import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 import 'package:flutter_app_crash_course_2/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+class _LoginPageState extends State<LoginPage>{
+  String name ="";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -14,7 +21,7 @@ class LoginPage extends StatelessWidget {
                   children: [
                     Image.asset("assets/images/login_image.png"),
                     SizedBox(height:20.0),
-                    Text('Welcome',style: TextStyle(fontSize: 30)),
+                    Text("Welcome $name",style: TextStyle(fontSize: 30)),
                     SizedBox(height:20.0),
                     Padding(
                         padding : const EdgeInsets.symmetric(vertical:10.0,horizontal:10.0),
@@ -25,6 +32,11 @@ class LoginPage extends StatelessWidget {
                                   hintText:"Enter Username",
                                   labelText:"Username",
                                 ),
+                                onChanged: (value){
+                                  setState(() {
+                                    name=value;
+                                  });
+                                },
                               ),
                               TextFormField(
                                 obscureText:true,
@@ -36,13 +48,35 @@ class LoginPage extends StatelessWidget {
                               SizedBox(
                                 height: 50,
                               ),
-                              ElevatedButton(
-                                child: Text('Enter'),
-                                style:TextButton.styleFrom(backgroundColor: Colors.blue,minimumSize: Size(150,50)),
-                                onPressed: (){
-                                    Navigator.pushNamed(context,MyRoutes.HomeRoute);
+                              InkWell(
+                                onTap: () async {
+                                  setState(() {
+                                    changeButton=true;
+                                  });
+                                  await Future.delayed(Duration(seconds:1));
+                                  Navigator.pushNamed(context,MyRoutes.HomeRoute);
                                 },
-                              )
+                                child:AnimatedContainer(
+                                  //  onTap:
+                                  duration: Duration(seconds:1),
+                                  height: 50,
+                                  width:changeButton?50:150,
+                                  alignment: Alignment.center,
+                                  child: changeButton?Icon(Icons.done):Text('Enter',style:TextStyle(color:Colors.white)),
+                                  decoration: BoxDecoration(
+                                    color: Colors.purple,
+                                    borderRadius: changeButton?BorderRadius.circular(50):BorderRadius.circular(10),
+                                  ),
+                                ),
+                              ),
+                              // ElevatedButton(
+                              //   child: Text('Enter'),
+                              //   style:TextButton.styleFrom(backgroundColor: Colors.blue,minimumSize: Size(150,50)),
+                              //   onPressed: (){
+                              //
+                              //     //  Navigator.pushNamed(context,MyRoutes.HomeRoute);
+                              //   },
+                              // )
                             ]
                         )
                     )
